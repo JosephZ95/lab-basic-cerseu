d6ef387cce6cc2f29d91db94876f9714084f1bb8
Joseph Zuñiga <jzunigago@ucvvirtual.edu.pe>
2023-06-15 21:54:16 -0500

    creación de archivos en mi repositorio

---
+++ Ejercicios1506/Tarea41.py
@@ -0,0 +1,13 @@
+var_1 = 'Joseph'
+var_2 = 'Zuniga'
+var_3 = 27
+var_4 = 1232.5
+
+#Concatenar
+print("Hola " + var_1 +" "+ var_2 +" su sueldo actual es de: "  + str(var_4) + " Soles" )
+
+var_5 = 1000
+var_6 = 'Hola papichulo'
+
+print("La suma de mis variables es: " + str(var_5) + var_6)
+
---
+++ Practica1.1/Practica1.py
@@ -0,0 +1,8 @@
+import sys
+
+Var1 = 2
+Var2 = 123
+Total = Var1 + Var2
+print(Total)
+
+print(sys.version)
